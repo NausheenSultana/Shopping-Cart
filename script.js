293ed847025067244bbe82c46bd9ea3e8e0b8816
Nausheen Sultana <nausheen.sultana0212@gmail.com>
2019-01-21 05:02:10 +0530

    latest

--- script.js
+++ script.js
@@ -2,28 +2,34 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
 	$scope.totalCartCost = 0;
 	$scope.discount = 0;
+	$scope.shipCharge = 0;
+	
+
 	console.log("home Controller");
 		  
 
   $http.get("data.json")
   .then(function(response) {
+	  $scope.pincode = response.data.pincode;
       $scope.items = response.data.products;
+	  console.log($scope.pincode);
 	  $scope.items.map(function(item) {
 		  item['qty'] = 1;
 	  })
 	  updateTotalCartCost();
-	  //$scope.count = 1;
-	 // $scope.pincode = response.data.pincode;
+
   });
-  $scope.deleteItem = function(id){
+  
+  
+  $scope.deleteItem = function(id){    /*** function to delete item entirely***/
 		$scope.items.splice(id, 1);
 		updateTotalCartCost();
 	  }
 	  
-	updateTotalCartCost = function(){
+	updateTotalCartCost = function(){   /*** function calculate total of item prices ***/
 		$scope.totalCartCost = 0;
 		$scope.items.forEach(function(item){
-		  $scope.totalCartCost += item.qty * item.price;
+		  $scope.totalCartCost += item.qty * item.price + $scope.shipCharge;
 	  })
 	  if($scope.totalCartCost > 5000) {
 		$scope.discount = 10 * $scope.totalCartCost / 100;
@@ -34,16 +40,40 @@ app.controller('myCtrl', function($scope, $http) {
 	  console.log("Total Cost: "+ $scope.totalCartCost);
 	}
 	
-	$scope.increaseQty = function(index) {
+	$scope.increaseQty = function(index) {  /*** function to increase quantity ***/
 		$scope.items[index].qty += 1;
 		updateTotalCartCost();
 	}
 	
-	$scope.decreaseQty = function(index) {
+	$scope.decreaseQty = function(index) {   /*** function to decrease quantity ***/
 		if($scope.items[index].qty > 0){
 			$scope.items[index].qty -= 1;
 			updateTotalCartCost();
 		}
+	}
+	$scope.delCost = function(pin) { 	/*** function for shipping chargess and est. delivery time ***/
+		$scope.flag = 1;       /** flag variable to enable checkout button only if pincode is valid **/
+		document.getElementById("chk").disabled = true;
+		angular.forEach($scope.pincode, function (value, key) {
+			//if($scope.pincode[pin].deliveryPrice>0)
+				if($scope.flag==1){
+						$scope.shipCharge = $scope.pincode[pin].deliveryPrice;
+					//else
+						//$scope.shipCharge = "Free";
+					if($scope.shipCharge>0)
+					{
+						$scope.flag = 0;
+						//document.getElementById("fd").style.display = none;
+					}	
+					
+					$scope.min = $scope.pincode[pin].estimatedDays.min;
+					$scope.max = $scope.pincode[pin].estimatedDays.max;
+					if($scope.totalCartCost>0)
+						document.getElementById("chk").disabled = false;
+					
+						
+				}
+	});
 	}
 }); 
 
@@ -52,7 +82,7 @@ app.controller('myCtrl', function($scope, $http) {
 
 
 
-function validateForm() {
+/* function validateForm() {
 	
 	
 		
@@ -130,7 +160,7 @@ function validateForm() {
 	//e.preventDefault();
     } 
 
-
+ */
 
 
 	
